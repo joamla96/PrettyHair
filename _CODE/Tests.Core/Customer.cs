413e858d4790f6756a9b6311ab2449d6840608a7
Jonas Laursen <jona8690@edu.eal.dk>
2017-01-15 02:46:04 +0100

    eh?

---
+++ _CODE/Tests.Core/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Core {
+	[TestClass]
+	class Customer {
+		CustomerRepository RepoCus = new CustomerRepository();
+
+		[TestMethod]
+		void CreateCustomer() {
+
+		}
+	}
+}
