ddcc39c4e0695bdc538f8bce36cdf76104cbe190
Jonas Laursen <jona8690@edu.eal.dk>
2017-01-15 03:05:35 +0100

    Check NextID Test

--- _CODE/Tests.Core/CustomerManager.cs
+++ _CODE/Tests.Core/CustomerManager.cs
@@ -24,7 +24,11 @@ namespace Tests.Core {
 
 		[TestMethod]
 		void CheckNextID() {
+			// First NextID should be 1
+			Assert.AreEqual(1, RepoCus.NextID());
 
+			RepoCus.CreateNewCustomer("John Doe", "jdoe@example.com", 20, "Fakestreet", "Faketown", 5000);
+			Assert.AreEqual(2, RepoCus.NextID());
 		}
 
 		[TestMethod]
